ca1ae472785a4ff4ebb81abda3d65e60c65dcdbe
Lecislav <lecislav@protonmail.com>
2023-06-09 18:47:22 +0200

    *

--- FilipP/src/app/app.component.ts
+++ FilipP/src/app/app.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'FilipP';
+  isNavbarExpanded: boolean = false;
+  toggleNavbar() {
+    this.isNavbarExpanded = !this.isNavbarExpanded;
+  }
 }
