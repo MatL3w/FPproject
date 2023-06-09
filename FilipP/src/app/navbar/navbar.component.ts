994b20467abe0a568f58f58c91275b3bf7bcaa1b
Lecislav <lecislav@protonmail.com>
2023-06-09 09:51:25 +0200

    *

--- FilipP/src/app/navbar/navbar.component.ts
+++ FilipP/src/app/navbar/navbar.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
 })
 export class NavbarComponent {
-
+  isFixed: boolean = false;
+  scrollDistance: number = 20;
+  @HostListener('window:scroll')
+  onScroll() {
+    if (window.pageYOffset > this.scrollDistance) {
+      this.isFixed = true;
+    } else {
+      this.isFixed = false;
+    }
+  }
 }
